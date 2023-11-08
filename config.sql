47edb107f054da5313c2bf50e8f4e97928677421
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-08 15:03:31 -0600

    Add new tables for monthly and annual fixed expenses, extra incomes, and interest calculator

--- config.sql
+++ config.sql
@@ -119,3 +119,58 @@ CREATE TABLE IF NOT EXISTS recordatorios (
     fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
 );
+
+-- Gastos Fijos Mensuales
+CREATE TABLE IF NOT EXISTS gastos_fijos_mensuales (
+    id_gasto_fijo_mensual INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    monto DECIMAL(10,2) NOT NULL,
+    dia_pago INT NOT NULL CHECK (dia_pago > 0 AND dia_pago <= 31),
+    activo TINYINT(1) NOT NULL DEFAULT 1,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
+);
+
+-- Gastos Fijos Anuales
+CREATE TABLE IF NOT EXISTS gastos_fijos_anuales (
+    id_gasto_fijo_anual INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    monto DECIMAL(10,2) NOT NULL,
+    mes_pago INT NOT NULL CHECK (mes_pago > 0 AND mes_pago <= 12),
+    dia_pago INT NOT NULL CHECK (dia_pago > 0 AND dia_pago <= 31),
+    activo TINYINT(1) NOT NULL DEFAULT 1,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
+);
+
+-- Ingresos Extras
+CREATE TABLE IF NOT EXISTS ingresos_extras (
+    id_ingreso_extra INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    monto DECIMAL(10,2) NOT NULL,
+    frecuencia ENUM('unico', 'recurrente') NOT NULL,
+    fecha_ingreso DATETIME NOT NULL,
+    activo TINYINT(1) NOT NULL DEFAULT 1,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
+);
+
+-- Calculadora de Intereses
+CREATE TABLE IF NOT EXISTS calculadora_intereses (
+    id_calculo_interes INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    id_deuda INT, -- Opcional, en caso de que se quiera asociar el cálculo a una deuda específica.
+    monto_principal DECIMAL(10,2) NOT NULL,
+    tasa_interes DECIMAL(5,2) NOT NULL,
+    tiempo_en_meses INT NOT NULL,
+    total_pagado DECIMAL(10,2) NOT NULL,
+    fecha_calculo TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE,
+    FOREIGN KEY (id_deuda) REFERENCES deudas(id_deuda) ON DELETE SET NULL -- Si se elimina la deuda, el cálculo no se elimina, pero se desvincula de la deuda.
+);
